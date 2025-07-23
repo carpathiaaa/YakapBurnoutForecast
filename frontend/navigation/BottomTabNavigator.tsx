0f395492fd07d4f885b300c6c5f0dfa8326da33a
carpathiaaa <charlesjung24@gmail.com>
2025-07-23 16:53:48 +0800

    Initialize frontend with Expo + base screens

---
+++ frontend/navigation/BottomTabNavigator.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import {
+  DashboardScreen,
+  PassportScreen,
+  BurnoutMirrorScreen,
+  EmotionalWeatherScreen
+} from '../screens';
+
+
+const Tab = createBottomTabNavigator();
+
+export default function BottomTabNavigator() {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="Dashboard" component={DashboardScreen} />
+      <Tab.Screen name="Passport" component={PassportScreen} />
+      <Tab.Screen name="Burnout Mirror" component={BurnoutMirrorScreen} />
+      <Tab.Screen name="Weather" component={EmotionalWeatherScreen} />
+    </Tab.Navigator>
+  );
+}
---
+++ frontend/screens/index.ts
@@ -0,0 +1,4 @@
+export { default as DashboardScreen } from './DashboardScreen';
+export { default as PassportScreen } from './PassportScreen';
+export { default as BurnoutMirrorScreen } from './BurnoutMirrorScreen';
+export { default as EmotionalWeatherScreen } from './EmotionalWeatherScreen';

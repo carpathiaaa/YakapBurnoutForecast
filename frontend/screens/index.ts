27794c39d58423563f69d6c35c44953e1885f500
carpathiaaa <charlesjung24@gmail.com>
2025-07-23 23:26:42 +0800

    feature toggle in index.ts

--- frontend/screens/index.ts
+++ frontend/screens/index.ts
@@ -1,4 +1,4 @@
-export { default as DashboardScreen } from './DashboardScreen';
 export { default as PassportScreen } from './PassportScreen';
-export { default as BurnoutMirrorScreen } from './BurnoutMirrorScreen';
-export { default as EmotionalWeatherScreen } from './EmotionalWeatherScreen';
+export const DashboardScreen = () => null;
+export const BurnoutMirrorScreen = () => null;
+export const EmotionalWeatherScreen = () => null;

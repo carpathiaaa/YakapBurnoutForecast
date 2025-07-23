15376e0f1eeda3a3c585a96152ac742d76490c77
carpathiaaa <charlesjung24@gmail.com>
2025-07-23 23:42:38 +0800

    test merger

--- frontend/screens/BurnoutMirrorScreen.tsx
+++ frontend/screens/BurnoutMirrorScreen.tsx
@@ -4,7 +4,7 @@ import { View, Text } from 'react-native';
 export default function BurnoutMirrorScreen() {
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>🪞 Burnout Mirror Screen</Text>
+      <Text>In Dev Burnout Mirror Screen</Text>
     </View>
   );
 }
